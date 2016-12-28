d723a6f7f0f7de8c639d03cb9ba5fdd015eec915
code4lifesabrinaperez <league@Mac-mini-2.local>
2016-12-28 11:50:56 -0800

    my day was good

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,32 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Caleb = new Robot();
+	Caleb.penDown();
+	Caleb.setPenColor(255,0,0);
+	Caleb.setSpeed(11);
+	for (int i = 0; i <400; i++) {
+		Caleb.turn(90);	
+	Caleb.move(200);
+	Caleb.turn(90);	
+	Caleb.move(200);
+	Caleb.turn(90);
+	Caleb.move(200);
+	Caleb.turn(90);
 
+
+	}
+ 	 
 	
-		
-		
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
+
+
